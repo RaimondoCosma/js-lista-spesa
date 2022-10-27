6e478bd8f74f3ce2c4bcc270ae17f2f5220ad9cf
RaimondoCosma <raimondo.cosma@gmail.com>
2022-10-27 13:46:15 +0200

    Crea array e variabile associata all'ul dell'html

--- js/my-script.js
+++ js/my-script.js
@@ -1,1 +1,11 @@
 'use strict';
+// Consegna:
+// Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
+
+// 1. Creo la lista della spesa tramite un array
+let shoppingList = ['Pane', 'Broccoli', 'Nutella', 'Birra', 'Pasta'];
+
+// 2. Dichiaro una variabile collegata all'id dell'ul del mio DOM
+const listContainer = document.querySelector('#list');
+
+
